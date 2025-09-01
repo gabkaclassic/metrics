74ec298b6d6caec993db2b751e8d584612293d08
errodion <r.kuzmin.w@gmail.com>
2025-09-02 00:30:31 +0300

    Router setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/gabkaclassic/metrics/internal/config"
+	"github.com/gabkaclassic/metrics/internal/handler"
+	"github.com/gabkaclassic/metrics/internal/repository"
+	"github.com/gabkaclassic/metrics/internal/service"
+	"github.com/gabkaclassic/metrics/internal/storage"
 	"github.com/gabkaclassic/metrics/pkg/httpserver"
 )
 
@@ -12,5 +16,23 @@ func main() {
 	server := httpserver.New(
 		httpserver.Address(cfg.Server.Address),
 	)
+
+	setupRouter(server)
+
 	server.Run()
+}
+
+func setupRouter(server *httpserver.Server) {
+
+	storage := storage.NewMemStorage()
+
+	// Metrics
+	metricsRepository := repository.NewMetricsRepository(storage)
+	metricsService := service.NewMetricsService(metricsRepository)
+	metricsHandler := handler.NewMetricsHandler(metricsService)
+
+	handler.SetupRouter(&handler.RouterConfiguration{
+		Mux:            server.GetHandler(),
+		MetricsHandler: metricsHandler,
+	})
 }
