509fe440809a8631dad6ff0c257700c564f7dc13
errodion <r.kuzmin.w@gmail.com>
2025-09-02 00:28:54 +0300

    Content type middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,12 @@
+package middleware
+
+import "net/http"
+
+func TextPlainContentType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+		handler.ServeHTTP(w, r)
+	})
+}
