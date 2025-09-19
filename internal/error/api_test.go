d0653a668074a3209762ab3212b2a87bf43caa52
errodion <r.kuzmin.w@gmail.com>
2025-09-19 17:13:08 +0300

    API errors tests

---
+++ internal/error/api_test.go
@@ -0,0 +1,47 @@
+package api_error
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRespondError(t *testing.T) {
+	tests := []struct {
+		name     string
+		err      error
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "ApiError",
+			err:      &ApiError{Code: 400, Message: "bad request"},
+			wantCode: 400,
+			wantBody: "bad request",
+		},
+		{
+			name:     "generic error",
+			err:      errors.New("some error"),
+			wantCode: http.StatusInternalServerError,
+			wantBody: "Internal server error",
+		},
+		{
+			name:     "nil error",
+			err:      nil,
+			wantCode: http.StatusInternalServerError,
+			wantBody: "Internal server error",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			RespondError(rr, tt.err)
+
+			assert.Equal(t, tt.wantCode, rr.Code)
+			assert.Equal(t, tt.wantBody, rr.Body.String())
+		})
+	}
+}
