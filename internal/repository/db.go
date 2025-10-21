d8712e8e8e94575e21157f9dd4f9e2ed0c4b64e8
errodion <r.kuzmin.w@gmail.com>
2025-10-22 01:28:55 +0300

    Memory and db repositories are splited

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -5,11 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"github.com/lib/pq"
-	"sync"
 	"time"
 
 	"github.com/gabkaclassic/metrics/internal/model"
-	"github.com/gabkaclassic/metrics/internal/storage"
 	"github.com/gabkaclassic/metrics/pkg/metric"
 )
 
@@ -17,156 +15,6 @@ const (
 	retriesAmount int           = 3
 	retryDelay    time.Duration = 1 * time.Second
 )
-
-type MetricsRepository interface {
-	Add(metric models.Metrics) error
-	AddAll(metrics *[]models.Metrics) error
-	ResetAll(metrics *[]models.Metrics) error
-	Reset(metric models.Metrics) error
-	Get(metricID string) (*models.Metrics, error)
-	GetAll() (*map[string]any, error)
-}
-
-type memoryMetricsRepository struct {
-	storage *storage.MemStorage
-	mutex   *sync.RWMutex
-}
-
-func NewMemoryMetricsRepository(storage *storage.MemStorage, mutex *sync.RWMutex) (MetricsRepository, error) {
-
-	if storage == nil {
-		return nil, errors.New("create new metrics repository failed: storage is nil")
-	}
-
-	return &memoryMetricsRepository{
-		storage: storage,
-		mutex:   mutex,
-	}, nil
-}
-
-func (repository *memoryMetricsRepository) GetAll() (*map[string]any, error) {
-
-	metrics := make(map[string]any, len(repository.storage.Metrics))
-
-	for id, m := range repository.storage.Metrics {
-		switch m.MType {
-		case string(metric.CounterType):
-			metrics[id] = *m.Delta
-		case string(metric.GaugeType):
-			metrics[id] = *m.Value
-		}
-	}
-
-	return &metrics, nil
-}
-
-func (repository *memoryMetricsRepository) Get(metricID string) (*models.Metrics, error) {
-
-	metric, exists := repository.storage.Metrics[metricID]
-
-	if !exists {
-		return nil, fmt.Errorf("metric %s not found", metricID)
-	}
-
-	return &metric, nil
-}
-
-func (repository *memoryMetricsRepository) updateMetric(metric models.Metrics, updateMetricFunction func(metric models.Metrics) error) error {
-	repository.mutex.Lock()
-	defer repository.mutex.Unlock()
-
-	if repository.storage.Metrics == nil {
-		repository.storage.Metrics = make(map[string]models.Metrics)
-	}
-
-	err := updateMetricFunction(metric)
-
-	return err
-}
-func (repository *memoryMetricsRepository) updateMetrics(metrics *[]models.Metrics, updateMetricsFunction func(metric *[]models.Metrics) error) error {
-	repository.mutex.Lock()
-	defer repository.mutex.Unlock()
-
-	if repository.storage.Metrics == nil {
-		repository.storage.Metrics = make(map[string]models.Metrics)
-	}
-
-	err := updateMetricsFunction(metrics)
-
-	return err
-}
-
-func (repository *memoryMetricsRepository) Add(metric models.Metrics) error {
-
-	err := repository.updateMetric(
-		metric,
-		func(metric models.Metrics) error {
-			if savedMetric, exists := repository.storage.Metrics[metric.ID]; exists {
-				*savedMetric.Delta = *(savedMetric.Delta) + *(metric.Delta)
-			} else {
-				repository.storage.Metrics[metric.ID] = metric
-			}
-			return nil
-		},
-	)
-
-	return err
-}
-
-func (repository *memoryMetricsRepository) AddAll(metrics *[]models.Metrics) error {
-
-	err := repository.updateMetrics(
-		metrics,
-		func(metrics *[]models.Metrics) error {
-			for _, metric := range *metrics {
-				if savedMetric, exists := repository.storage.Metrics[metric.ID]; exists {
-					*savedMetric.Delta = *(savedMetric.Delta) + *(metric.Delta)
-				} else {
-					repository.storage.Metrics[metric.ID] = metric
-				}
-			}
-			return nil
-		},
-	)
-
-	return err
-}
-
-func (repository *memoryMetricsRepository) Reset(metric models.Metrics) error {
-
-	err := repository.updateMetric(
-		metric,
-		func(metric models.Metrics) error {
-			if savedMetric, exists := repository.storage.Metrics[metric.ID]; exists {
-				*savedMetric.Value = *(metric.Value)
-			} else {
-				repository.storage.Metrics[metric.ID] = metric
-			}
-			return nil
-		},
-	)
-
-	return err
-}
-
-func (repository *memoryMetricsRepository) ResetAll(metrics *[]models.Metrics) error {
-
-	err := repository.updateMetrics(
-		metrics,
-		func(metrics *[]models.Metrics) error {
-			for _, metric := range *metrics {
-				if savedMetric, exists := repository.storage.Metrics[metric.ID]; exists {
-					*savedMetric.Value = *(metric.Value)
-				} else {
-					repository.storage.Metrics[metric.ID] = metric
-				}
-			}
-			return nil
-		},
-	)
-
-	return err
-}
 
 type dbMetricsRepository struct {
 	storage *sql.DB
