7c086ab61991874758be3e7868229a54595b0b75
errodion <r.kuzmin.w@gmail.com>
2025-12-20 02:39:42 +0300

    HTTP server doc

--- pkg/httpserver/option.go
+++ pkg/httpserver/option.go
@@ -4,14 +4,17 @@ import (
 	"net/http"
 )
 
+// Option represents a functional option for Server configuration.
 type Option func(*Server)
 
+// Address sets the server listen address.
 func Address(address string) Option {
 	return func(server *Server) {
 		server.address = address
 	}
 }
 
+// Handler sets the HTTP handler for the server.
 func Handler(handler *http.Handler) Option {
 	return func(server *Server) {
 		server.handler = handler
